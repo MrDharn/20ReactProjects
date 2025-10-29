5364708dc25ed029d037b6ef05aa5917cc7eed15
Dharn <mydharn'gmail.com>
2025-10-29 15:42:24 +0000

    QRCodeGenerator included

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,11 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import App from "./RecursiveNav/App.jsx";
 import {navData} from '../src/RecursiveNav/assets/navData.js'
+import QRcodeGenerator from "./QRcodeGenerator/components/QRcodeGenerator.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
-      <App menus = {navData}/>
+      {/* <App menus = {navData}/> */}
+      <QRcodeGenerator/>
   </StrictMode>
 );
