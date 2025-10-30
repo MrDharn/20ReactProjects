3cce4934b825d46d42067975a79200f14d0e8e7c
Dharn <mydharn'gmail.com>
2025-10-30 21:12:59 +0000

    updated main

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,15 @@ import { createRoot } from "react-dom/client";
 import App from "./RecursiveNav/App.jsx";
 import {navData} from '../src/RecursiveNav/assets/navData.js'
 import QRcodeGenerator from "./QRcodeGenerator/components/QRcodeGenerator.jsx";
+import DarkLightMode from "./DarkLightMode/components/DarkLightMode.jsx";
+import ScrollBar from "./ScrollBarIndicator/components/ScrollBar.jsx";
 
 createRoot(document.getElementById("root")).render(
   <StrictMode>
       {/* <App menus = {navData}/> */}
-      <QRcodeGenerator/>
+      {/* <QRcodeGenerator/> */}
+      {/* <DarkLightMode/> */}
+
+      <ScrollBar url='https://dummyjson.com/products/category/smartphones'/>
   </StrictMode>
 );
