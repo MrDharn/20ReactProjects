c45e4c6d3ee32d1a30fb715be18c5fc10f0c2753
Dharn <mydharn'gmail.com>
2025-11-05 03:38:43 +0000

    data holding the values

---
+++ src/scrollToSection/data.js
@@ -0,0 +1,7 @@
+export const data = [
+  { label: 'Home', style: { background: '#ff4757' } },
+  { label: 'About', style: { background: '#1e90ff' } },
+  { label: 'Services', style: { background: '#2ed573' } },
+  { label: 'Portfolio', style: { background: '#ffa502' } },
+  { label: 'Contact', style: { background: '#3742fa' } }
+];
